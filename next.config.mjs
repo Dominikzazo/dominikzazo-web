ec16a6fd453f311be06741acdef87747b3fa3004
Dominik Zazo <zazodominik@gmail.com>
2026-06-07 17:34:43 +0200

    Add /vecernik and /kompas path proxies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/vecernik', destination: 'https://vecernik.vercel.app/vecernik' },
+      { source: '/vecernik/:path*', destination: 'https://vecernik.vercel.app/vecernik/:path*' },
+      { source: '/kompas', destination: 'https://kompas-one.vercel.app/kompas' },
+      { source: '/kompas/:path*', destination: 'https://kompas-one.vercel.app/kompas/:path*' },
+    ]
+  },
+}
 
 export default nextConfig
